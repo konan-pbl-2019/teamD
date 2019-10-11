99048750305a9c6f9d6384e40bac42494e5d9440
s1871081 <s1871081@d-13102-068.ku-kinds.konan-u.ac.jp>
2019-10-11 16:57:19 +0900

    微修正

--- src/template/quiz2D/TemplateQuizGame.java
+++ src/template/quiz2D/TemplateQuizGame.java
@@ -1,35 +1,23 @@
 package template.quiz2D;
 
 import java.awt.Color;
-import java.math.BigDecimal;
-
-
 
 import framework.RWT.RWTContainer;
 import framework.RWT.RWTFrame3D;
 import framework.RWT.RWTVirtualController;
-import framework.game2D.Sprite;
-import framework.gameMain.BaseScenarioGameContainer;
-import framework.gameMain.SimpleRolePlayingGame;
 import framework.gameMain.SimpleScenarioGame;
-import framework.model3D.ModelFactory;
-import framework.model3D.Object3D;
-import framework.model3D.Position3D;
-import framework.model3D.Quaternion3D;
 import framework.model3D.Universe;
-import framework.physics.PhysicsUtility;
 import framework.scenario.Event;
-import framework.scenario.ScenarioManager;
 import framework.scenario.ScenarioState;
 import framework.view3D.Camera3D;
 
 public class TemplateQuizGame extends SimpleScenarioGame {
 	private RWTFrame3D frame;
-	
+
 	@Override
-	public void init(Universe universe, Camera3D camera) {		
+	public void init(Universe universe, Camera3D camera) {
 		// シナリオの設定
-		setScenario("data\\TemplateQuiz\\scenario.xml");
+		setScenario("data\\TemplateQuiz\\scenario2.xml");
 		container.setScenario(scenario);
 		scenario.fire("開始");
 	}
@@ -42,17 +30,17 @@ public class TemplateQuizGame extends SimpleScenarioGame {
 		frame.setBackground(Color.BLACK);
 		return frame;
 	}
-	
+
 	@Override
 	protected RWTContainer createRWTContainer() {
 		container = new QuizGameContainer();
 		return container;
 	}
-	
+
 	@Override
 	public void progress(RWTVirtualController virtualController, long interval) {
 	}
-	
+
 	@Override
 	public void showOption(int n, String option) {
 		((QuizGameContainer)container).showOption(n, option);
@@ -68,7 +56,7 @@ public class TemplateQuizGame extends SimpleScenarioGame {
 
 	/**
 	 * ゲームのメイン
-	 * 
+	 *
 	 * @param args
 	 */
 	public static void main(String[] args) {
