f33e2b88191b6841b9b5c03e3cec48953a742b67
s1871055 <s1871055@d-13102-067.ku-kinds.konan-u.ac.jp>
2019-10-18 13:53:43 +0900

    絵の表示

---
+++ src/template/quiz2D/HpManager.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package template.quiz2D;
+
+/**
+ * @author Takayama
+ *
+ */
+public class HpManager {
+	private int hp;
+	
+	public static int Hp(int hp)
+	{
+		
+		return hp;
+	}
+	
+
+}
