558a8fec4d867e1d09e4cfdd74e3f83b8cdd6c15
s1871055 <s1871055@d-13102-067.ku-kinds.konan-u.ac.jp>
2019-10-25 16:31:54 +0900

    コメントの追加

--- src/template/quiz2D/QuizGameContainer.java
+++ src/template/quiz2D/QuizGameContainer.java
@@ -325,6 +325,10 @@ public class QuizGameContainer extends BaseScenarioGameContainer {
 
 	}
 
+	//**************************
+
+
+	//*****画面表示関連*****//
 	//ゲームオーバーの表示
 	public void retire() {
 		gameover.setVisible(true);
@@ -344,4 +348,5 @@ public class QuizGameContainer extends BaseScenarioGameContainer {
 		enemy2.setVisible(true);
 	}
 
+	//*****************************:
 }
